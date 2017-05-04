e818b0315f046e309284d3ea035fb9f78eddd9f5
Alfredo Oltra <alfredo.ptcf@gmail.com>
2017-05-04 20:14:04 +0200

    Creación carpetas por cada encabezado de nivel 1

--- mentor.py
+++ mentor.py
@@ -9,23 +9,40 @@ Conversor de odt a html para incorporarlo en Moodle
 
 import os
 import zipfile
+from bs4 import BeautifulSoup
 
 def unzip_odt(odt_file):
     """
     unzip de odt file
     """
     directory_to_extract = os.path.splitext(odt_file)[0]
-    directory_to_extract += ".mentor"
+    directory_to_extract += ".mentor.tmp"
     zip_ref = zipfile.ZipFile(odt_file, 'r')
     zip_ref.extractall(directory_to_extract)
     zip_ref.close()
+    return directory_to_extract
+
 
 
 def main():
     """
     main function
     """
-    unzip_odt("prueba1.zip")
+    filename = "prueba1.odt"
+    directory_source = unzip_odt(filename)
+
+    file = open(directory_source + "/content.xml", 'r')
+    doc = BeautifulSoup(file, "lxml")
+
+    # unit structure
+    directory_target = os.path.splitext(filename)[0] + ".mentor"
+    if not os.path.exists(directory_target):
+        os.makedirs(directory_target)
+
+    blocks_l1 = doc.findAll('text:h', attrs={"text:outline-level" : "1"})
+    for i in range(len(blocks_l1)):
+        os.makedirs(directory_target + "/l1_" + str(i+1))
+
     return
 
 
