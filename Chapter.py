3f6fcc86d9ca78ec59e267d220a7d47e751eb6e1
Alfredo Oltra <alfredo.ptcf@gmail.com>
2017-05-06 10:47:45 +0200

    combo en la navbar para el acceso a los bloques de nivel 1

---
+++ Chapter.py
@@ -0,0 +1,15 @@
+"""
+Implementation Chapter class
+"""
+#
+# Author: Alfredo Oltra
+# Date: 3/5/2017
+#
+# License GPL-3.0
+
+class Chapter(object):
+    
+    def __init__(self, number, block):
+        self.number = number
+        self.block = block
+        
--- mentor.py
+++ mentor.py
@@ -2,15 +2,17 @@
 Conversor de odt a html para incorporarlo en Moodle
 """
 #
-# Autor: Alfredo Oltra
-# Fecha: 3/5/2017
+# Author: Alfredo Oltra
+# Date: 3/5/2017
 #
-# Licencia GPL-3.0
+# License GPL-3.0
 
 import os
 import zipfile
 from bs4 import BeautifulSoup
 from genshi.template import TemplateLoader
+
+import Chapter
 
 # a eliminar en un futuro
 import shutil
@@ -45,25 +47,27 @@ def main():
     if os.path.exists(directory_target):
         shutil.rmtree(directory_target)
 
-    blocks_l1 = doc.findAll('text:h', attrs={"text:outline-level" : "1"})
-    for i in range(len(blocks_l1)):
-        os.makedirs(directory_target + "/l1_" + str(i+1))
+    blocks_l1 = []
 
+    blocks = doc.findAll('text:h', attrs={"text:outline-level" : "1"})
+    for idx, block in enumerate(blocks, start=1):
+        os.makedirs(directory_target + "/l1_" + str(idx))
+        blocks_l1.append(Chapter.Chapter(idx, block))
+
+    blocks = []
 
     # generating units
     loader = TemplateLoader(os.path.join(os.path.dirname(__file__), 'templates/basic'),
                             auto_reload=True)
-    tmpl = loader.load('unit.html')
+    tmpl = loader.load('chapter.html')
 
-   # for idx, block in enumerate(blocks_l1, start=0):
     for idx, block in enumerate(blocks_l1, start=1):
-        filename_unit = directory_target + "/l1_" + str(idx) + "/unit.html"
+        filename_unit = directory_target + "/l1_" + str(idx) + "/chapter.html"
         with open(filename_unit, 'w') as file_unit:
-            file_unit.write(tmpl.generate(title=block.string,
-                                          lang="es").render('html', doctype='html5'))
+            file_unit.write(tmpl.generate(title=block.block.string,
+                                          lang="es", uts=blocks_l1).render('html', doctype='html5'))
 
     return
-
 
 
 if __name__ == "__main__":
